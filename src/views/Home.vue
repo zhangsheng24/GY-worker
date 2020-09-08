6cd015a0a8c36807a3a40867e39265053a627211
zhangsheng <1070683532@qq.com>
2020-09-08 18:08:31 +0800

    增加2个测试页面，。后期会修改

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="home">首页</div>
+</template>
+<style lang="scss" scoped>
+.home{
+    height: 100vh;
+    width: 100vw;
+    background: red;
+}
+</style>
---
+++ src/views/User.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="user">我的</div>
+</template>
+<style lang="scss" scoped>
+.user{
+    height: 100vh;
+    width: 100vw;
+    background: pink;
+}
+</style>
