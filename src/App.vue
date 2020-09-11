557eebaebc590aa61139c2eb6a0578d5f5d14eb2
zhangsheng <1070683532@qq.com>
2020-09-11 18:06:58 +0800

    直接weixin-js-skd验证

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,9 @@
 
 <script>
 import TabBar from "@/commons/template/TabBar";
-
+import {LgetItem} from '@/utils/storage'
+import {isIos} from '@/utils/index'
+import api from '@/api'
 export default {
   data() {
     return {
@@ -27,6 +29,54 @@ export default {
   components: {
     TabBar,
   },
+  watch:{
+    $route(to,from){
+      if(LgetItem('jwtToken')){
+        this.$wx.$wxInit=this.wxInit
+        //如果是从根路径加载，也就是说在ios环境下，需要且只能在网站第一次打开的时候初始化config配置。
+        //如果在需要的地方配置config，然后在其他页面进入到这个页面的时候使用wx的jssdk就会报错
+        //但如果在Android环境下，就不需要在根路径去配置config，而是哪个页面需要就在哪个页面配置
+        if(from.path === '/' && isIos()){
+          this.$wx.$wxInit()
+        }
+      }
+    }
+  },
+  methods:{
+    wxInit(){
+      return new Promise((resolve,reject)=>{
+        //如果是ios环境，并且已经权限验证成功就不用再去通过config接口注入权限验证配置
+        if(isIos() && this.$wx.readySucc){
+          resolve()
+          return
+        }
+        api('getWxSign',{
+          url:encodeURIComponent(window.location.href.split('#')[0])
+        }).then(res=>{
+          this.$wx.config({
+            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            appId: res.appId, // 必填，公众号的唯一标识
+            timestamp: res.timestamp, // 必填，生成签名的时间戳
+            nonceStr: res.nonceStr, // 必填，生成签名的随机串
+            signature: res.signature,// 必填，签名
+            jsApiList: ['getLocation'] // 必填，需要使用的JS接口列表
+          })
+          this.$wx.ready(_=>{
+            this.$wx.readySucc=true
+            resolve()
+            console.log(_)
+          })
+          this.$wx.error(_=>{
+            reject()
+            console.log(_)
+          })
+        }).catch(err=>{
+          reject()
+          console.log(err)
+        })
+      })
+    }
+  }
 };
 </script>
 <!-- 之前出现的滚动条问题是因为viewport，加上就ok了，所以说在开发移动端网站的时候一定要注意加上viewport -->
--- src/main.js
+++ src/main.js
@@ -7,8 +7,10 @@ import Vant from 'vant'
 import 'amfe-flexible'
 import 'vant/lib/index.css'
 import './permission'
+const wx = require('weixin-js-sdk');
 
 Vue.config.productionTip = false
+Vue.prototype.$wx=wx
 
 Vue.use(Vant)
 
@@ -19,3 +21,24 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+//网页默认不添加扩展的表现是 viewport-fit=contain，需要适配 iPhoneX 必须设置 viewport-fit=cover，这是适配的关键步骤。https://blog.csdn.net/qq_42354773/article/details/81018615
+
+//主要就是因为iphonex
+//iOS11 新增特性，Webkit 的一个 CSS 函数，用于设定安全区域与边界的距离，有四个预定义的变量：
+//safe-area-inset-left：安全区域距离左边边界距离
+//safe-area-inset-right：安全区域距离右边边界距离
+//safe-area-inset-top：安全区域距离顶部边界距离
+//safe-area-inset-bottom：安全区域距离底部边界距离
+//这里我们只需要关注 safe-area-inset-bottom 这个变量，因为它对应的就是小黑条的高度
+//padding-bottom: constant(safe-area-inset-bottom); /* 兼容 iOS < 11.2 */
+//padding-bottom: env(safe-area-inset-bottom); /* 兼容 iOS >= 11.2 */
+// env() 跟 constant() 需要同时存在，而且顺序不能换。
+
+
+
+
+//关于解决history刷新404的问题
+//刷新404，为什么，因为本地路径中没有这个真实资源存在，这些访问资源都是在js里渲染的。
+//在nginx捕获，返回到 index.html 页面，这个页面就是你 app 依赖的页面。
+//然后前端就会根据路由拦截，然后跳转到我们当前刷新的这个页面路径
